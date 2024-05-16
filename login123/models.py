eca524f8ac54d59346be0e8aae3651a6b4095051
manhnguyen12342 <manhvui123321@gmail.com>
2024-05-16 10:04:14 +0700

    done register+login

--- login123/models.py
+++ login123/models.py
@@ -13,7 +13,7 @@ class User(AbstractBaseUser):
     )
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=255 , unique = True)
-    password = models.CharField(max_length=100, write_only = True)
+    password = models.CharField(max_length=100)
     username = None
     
     USERNAME_FIELD = 'email'
